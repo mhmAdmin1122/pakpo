15894ff40982f29cd32000803a25066fd398a988
mhmAdmin1122 <hasaanad12min@gmail.com>
2023-06-14 20:12:06 +0500

    feww work on errors

--- pages/components/Card.tsx
+++ pages/components/Card.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-const Card = ({ data }) => {
+const Card = ({ data }:any) => {
   useEffect(() => {
     AOS.init({
       // Customize AOS options here (easing, duration, etc.)
